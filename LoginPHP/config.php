e140bb2b18c6fb2db79e68c3f0cb294683e02866
datp01 <97713597+datp01@users.noreply.github.com>
2022-09-18 20:37:39 +0700

    1

---
+++ LoginPHP/config.php
@@ -0,0 +1,8 @@
+<?php
+     $dataHost = 'localhost';
+     $dataName ='user';
+     $dataUserName = 'root';
+     $dataPassword = '';
+
+     $mysqli = mysqli_connect($dataHost,$dataUserName,$dataPassword,$dataName);
+?>
